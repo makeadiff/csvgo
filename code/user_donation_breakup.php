a324a613e5459a302c487b93162bd5f315a25644
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2016-12-13 16:40:28 +0530

    Left join for user_donation_breakup

--- code/user_donation_breakup.php
+++ code/user_donation_breakup.php
@@ -3,24 +3,26 @@ if(isset($_SERVER['HTTP_HOST']) and $_SERVER['HTTP_HOST'] == 'makeadiff.in') {
 	$db_donut = 'makeadiff_cfrapp';
 	$db_madapp = 'makeadiff_madapp';
 } else {
-	$db_donut = 'Project_Donut';
-	$db_madapp = 'Project_Madapp';
+	$db_donut = 'makeadiff_cfrapp';
+	$db_madapp = 'makeadiff_cfrapp';
 }
 
-$donut_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.donation_amount) AS amount, C.name AS city
+$donut_data = $sql->getById("SELECT U.id,MU.email,MU.phone,COALESCE(SUM(D.donation_amount),0) AS amount, C.name AS city
 			FROM $db_donut.users U 
 			INNER JOIN $db_donut.cities C ON C.id=U.city_id
 			INNER JOIN $db_madapp.User MU ON MU.id=U.madapp_user_id
-			INNER JOIN $db_donut.donations D ON U.id=D.fundraiser_id
-			WHERE D.created_at > '2016-10-01 00:00:00'
-			GROUP BY D.fundraiser_id");
-$exdon_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.amount) AS amount, C.name AS city
+			LEFT JOIN $db_donut.donations D ON U.id=D.fundraiser_id AND D.created_at > '2016-10-01 00:00:00'
+			WHERE U.is_deleted = 0
+			GROUP BY U.id
+			ORDER BY amount DESC");
+$exdon_data = $sql->getById("SELECT U.id,MU.email,MU.phone,COALESCE(SUM(D.amount),0) AS amount, C.name AS city
 			FROM $db_donut.users U 
 			INNER JOIN $db_donut.cities C ON C.id=U.city_id
 			INNER JOIN $db_madapp.User MU ON MU.id=U.madapp_user_id
-			INNER JOIN $db_donut.external_donations D ON U.id=D.fundraiser_id
-			WHERE D.created_at > '2016-10-01 00:00:00'
-			GROUP BY D.fundraiser_id");
+			LEFT JOIN $db_donut.external_donations D ON U.id=D.fundraiser_id AND D.created_at > '2016-10-01 00:00:00'
+			WHERE U.is_deleted = 0
+			GROUP BY U.id
+			ORDER BY amount DESC");
 
 $data = $donut_data;
 
