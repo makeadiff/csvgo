083184244aae52848adfed70d0e4e52287a5414e
Binny V A <binnyva@gmail.com>
2016-12-13 12:33:05 +0530

    Added the check by date - hardcoded

--- code/user_donation_breakup.php
+++ code/user_donation_breakup.php
@@ -12,26 +12,20 @@ $donut_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.donation_amount
 			INNER JOIN $db_donut.cities C ON C.id=U.city_id
 			INNER JOIN $db_madapp.User MU ON MU.id=U.madapp_user_id
 			INNER JOIN $db_donut.donations D ON U.id=D.fundraiser_id
+			WHERE D.created_at > '2016-10-01 00:00:00'
 			GROUP BY D.fundraiser_id");
 $exdon_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.amount) AS amount, C.name AS city
 			FROM $db_donut.users U 
 			INNER JOIN $db_donut.cities C ON C.id=U.city_id
 			INNER JOIN $db_madapp.User MU ON MU.id=U.madapp_user_id
 			INNER JOIN $db_donut.external_donations D ON U.id=D.fundraiser_id
+			WHERE D.created_at > '2016-10-01 00:00:00'
 			GROUP BY D.fundraiser_id");
 
-$data = array();
-foreach ($donut_data as $user_id => $row) {
-	if(isset($exdon_data[$user_id])) {
-		$row['amount'] += $exdon_data[$user_id]['amount'];
-	}
+$data = $donut_data;
 
-	$data[] = $row;
+foreach ($exdon_data as $id => $value) {
+	if(isset($data[$id])) $data[$id]['amount'] += $exdon_data[$id]['amount'];
+	else $data[$id] = $exdon_data[$id];
 }
 
-$difference = array_diff_key($exdon_data, $donut_data);
-foreach ($difference as $user_id => $row) {
-	$data[] = $row;	
-}
-
-// print json_encode($data);
