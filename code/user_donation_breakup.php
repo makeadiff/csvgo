10aacb06bc0af1eeea3dda72dedc7236690bb7e1
Binny V A <binnyva@gmail.com>
2017-02-02 15:15:40 +0530

    Small change to code

--- code/user_donation_breakup.php
+++ code/user_donation_breakup.php
@@ -7,14 +7,14 @@ if(isset($_SERVER['HTTP_HOST']) and $_SERVER['HTTP_HOST'] == 'makeadiff.in') {
 	$db_madapp = 'Project_Madapp';
 }
 
-$donut_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.donation_amount) AS amount, C.name AS city
+$donut_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.donation_amount) AS amount, C.name AS city, MU.first_name
 			FROM $db_donut.users U 
 			INNER JOIN $db_donut.cities C ON C.id=U.city_id
 			INNER JOIN $db_madapp.User MU ON MU.id=U.madapp_user_id
 			INNER JOIN $db_donut.donations D ON U.id=D.fundraiser_id
 			WHERE D.created_at > '2016-10-01 00:00:00'
 			GROUP BY D.fundraiser_id");
-$exdon_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.amount) AS amount, C.name AS city
+$exdon_data = $sql->getById("SELECT U.id,MU.email,MU.phone,SUM(D.amount) AS amount, C.name AS city, MU.first_name
 			FROM $db_donut.users U 
 			INNER JOIN $db_donut.cities C ON C.id=U.city_id
 			INNER JOIN $db_madapp.User MU ON MU.id=U.madapp_user_id
