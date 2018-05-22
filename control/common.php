278b74b92ed4c9c1dc6b6b584d6869a3ff50deff
Binny V A <binnyva@gmail.com>
2018-05-22 17:39:00 +0530

    Removed the explcit setting of URL

--- control/common.php
+++ control/common.php
@@ -2,12 +2,6 @@
 require('../../common.php');
 require_once('../includes/application.php');
 
-if($config['server_host'] == 'localhost') {
-	$config['site_url'] = 'http://localhost/MAD/';
-} else {
-	$config['site_url'] = 'http://makeadiff.in/';
-}
-
 $config['site_folder'] = dirname(__FILE__);
 $template->page = str_replace("control/", "", $template->page);
 $template->css_folder = 'control/css';
