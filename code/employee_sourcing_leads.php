85789250d1c3e8b9013249a128d47cc0a72a9c2f
Binny V A <binnyva@gmail.com>
2019-01-17 16:52:23 +0530

    ADded more info

--- code/employee_sourcing_leads.php
+++ code/employee_sourcing_leads.php
@@ -1,15 +1,17 @@
 <?php
 $all_verticals = $sql->getById("SELECT id,name FROM Vertical WHERE status='1'");
+$all_cities = $sql->getById("SELECT id,name FROM City WHERE type='actual'");
 $data = $sql->getAll("SELECT name,phone,email,experience,user_id,applied_role,applied_verticals,cv_file,cover_letter FROM Temp_Employee_Signup");
 foreach ($data as $index => $row) {
 	if($row['user_id']) {
-		$user = $sql->getAssoc("SELECT name,phone,email,user_type,joined_on,left_on FROM User WHERE id=" . $row['user_id']);
+		$user = $sql->getAssoc("SELECT name,phone,email,user_type,joined_on,left_on,city_id FROM User WHERE id=" . $row['user_id']);
 		$roles = $sql->getAll("SELECT GROUP_CONCAT(',', G.name) AS roles,UG.year FROM `Group` G INNER JOIN UserGroup UG ON UG.group_id=G.id WHERE UG.user_id=" . $row['user_id'] . " GROUP BY UG.year");
 
 		$row['name'] = $user['name'];
 		$row['phone'] = $user['phone'];
 		$row['email'] = $user['email'];
-		$row['volunteer_info'] = "Type: " . $user['user_type'] . "\nJoined On: " . $user['joined_on'] . "\n";
+		$row['user_id'] = "https://makeadiff.in/madapp/index.php/user/view/" . $row['user_id'];
+		$row['volunteer_info'] = "City: " . $all_cities[$user['city_id']] . "\nType: " . $user['user_type'] . "\nJoined On: " . $user['joined_on'] . "\n";
 		if($user['left_on'] and $user['left_on'] != '0000-00-00') $row['volunteer_info'] .= "Left On: " . $user['left_on'] . "\n";
 
 		$all_roles = '';
