5ea2a9a360f3fe04c3868d21512daf567d135cd9
Binny V A <binnyva@gmail.com>
2019-03-30 12:44:31 +0530

    Fixed issue with missing UI for html mime data view

--- control/index.php
+++ control/index.php
@@ -3,9 +3,10 @@ require './common.php' ;
 
 $crud = new Crud('App_CSVGo');
 $crud->addField("name", "Name", 'varchar',array(), array('text'=>'$row["name"]', 'url'=> '"sql.php?name=" . $row["name"]'), 'text', 'url');
-$crud->addField("last_run_on", "CSV", 'varchar',array(), array('text'=>'"CSV Link"', 'url'=> '"../index.php?name=" . $row["name"]'), 'text', 'url');
+$crud->addField("csv_link", "CSV", 'varchar',array(), array('text'=>'"CSV Link"', 'url'=> '"../index.php?name=" . $row["name"]'), 'text', 'url');
+$crud->addField("data_link", "View Data", 'varchar',array(), array('text'=>'"View Data"', 'url'=> '"../index.php?mime=html&name=" . $row["name"]'), 'text', 'url');
 
 $crud->setListingQuery("SELECT * FROM App_CSVGo WHERE status='1'");
-$crud->setListingFields("name", "vertical_id", "description", "added_on", "last_run_on", 'status');
+$crud->setListingFields("name", "description", "added_on", "csv_link", 'data_link', 'status');
 
 render();
--- templates/html.php
+++ templates/html.php
@@ -1,5 +1,5 @@
-<?php if($data) { ?>
-<h2>Results...</h2>
+<?php if($data) { ?><br /><br />
+<h1><?php echo format($name); ?></h1>
 
 <?php
 if($pager) {
@@ -32,4 +32,15 @@ foreach ($data as $row) {
 ?>
 </table>
 
+<?php
+if($pager) {
+	$pager->link_template = '<a href="%%PAGE_LINK%%" class="page-%%CLASS%%"><img alt="%%TEXT%%" src="images/icons/arrows/%%CLASS%%.png" /></a>';
+	if($pager->total_pages > 1) {
+		print $pager->getLink("first") . $pager->getLink("back");
+		$pager->printPager();
+		print $pager->getLink("next") . $pager->getLink("last") . '<br />';
+	}
+	if($pager->total_items) print $pager->getStatus();
+}
+?>
 <?php } ?>
