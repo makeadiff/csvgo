fdad8e80dd319bbe79291d3a54b8b1550e6bb4d2
Binny V A <binnyva@gmail.com>
2020-11-05 17:21:19 +0530

    Added a custom CSV - convert a Zoho JSON to csv

---
+++ code/zoho_visibility_tracker.php
@@ -0,0 +1,8 @@
+<?php
+// Purpose: Convert JSON to CSV
+$response = load('https://creator.zoho.com/api/json/mad-recruit/view/Visibility_Tracker1?authtoken=cdcfd4eb1b77b0835f4339827906e42a&scope=creatorapi', ['cache' => false]);
+
+$json = str_replace(['var zohojithincn1view3117 = ', '}]};'], ['', '}]}'], $response);
+$data_raw = json_decode($json, true);
+$data = $data_raw['Registration'];
+// dump($data); exit;
