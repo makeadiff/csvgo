8a08cad5c7c8844160822bfdbcf0fbd1d540a91b
Binny V A <binnyva@gmail.com>
2020-11-12 17:30:38 +0530

    Made this cached

--- code/zoho_visibility_tracker.php
+++ code/zoho_visibility_tracker.php
@@ -2,7 +2,7 @@
 ini_set('max_execution_time', '300');
 
 // Purpose: Convert JSON to CSV
-$response = load('https://creator.zoho.com/api/json/mad-recruit/view/Visibility_Tracker1?authtoken=cdcfd4eb1b77b0835f4339827906e42a&scope=creatorapi', ['cache' => false]);
+$response = load('https://creator.zoho.com/api/json/mad-recruit/view/Visibility_Tracker1?authtoken=cdcfd4eb1b77b0835f4339827906e42a&scope=creatorapi', ['cache' => true, 'cache_timeout' => 60 * 24]);
 
 $json = str_replace(['var zohojithincn1view3117 = ', '}]};'], ['', '}]}'], $response);
 $data_raw = json_decode($json, true);
