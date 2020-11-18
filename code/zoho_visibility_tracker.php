22661843c58bc8000c9864272fa8bd3ac8264328
Binny V A <binnyva@gmail.com>
2020-11-18 18:21:27 +0530

    Added No Cache Support

--- code/zoho_visibility_tracker.php
+++ code/zoho_visibility_tracker.php
@@ -1,8 +1,11 @@
 <?php
 ini_set('max_execution_time', '300');
 
+$cache = true;
+if(i($QUERY, 'no_cache', false)) $cache = false;
+
 // Purpose: Convert JSON to CSV
-$response = load('https://creator.zoho.com/api/json/mad-recruit/view/Visibility_Tracker1?authtoken=cdcfd4eb1b77b0835f4339827906e42a&scope=creatorapi', ['cache' => true, 'cache_timeout' => 60 * 24]);
+$response = load('https://creator.zoho.com/api/json/mad-recruit/view/Visibility_Tracker1?authtoken=cdcfd4eb1b77b0835f4339827906e42a&scope=creatorapi', ['cache' => $cache, 'cache_timeout' => 60 * 24]);
 
 $json = str_replace(['var zohojithincn1view3117 = ', '}]};'], ['', '}]}'], $response);
 $data_raw = json_decode($json, true);
