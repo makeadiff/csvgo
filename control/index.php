070c90d1409c47891794d1f8823749341b0fc62b
Binny V A <binnyva@gmail.com>
2021-09-23 15:40:08 +0530

    Better Sorting of data.

--- control/index.php
+++ control/index.php
@@ -7,7 +7,7 @@ $crud->addField("name", "Name", 'varchar',array(), array('text'=>'$row["name"]',
 $crud->addField("csv_link", "CSV", 'varchar',array(), array('text'=>'"CSV Link"', 'url'=> '"../index.php?name=" . $row["name"]'), 'text', 'url');
 $crud->addField("data_link", "View Data", 'varchar',array(), array('text'=>'"View Data"', 'url'=> '"../index.php?mime=html&name=" . $row["name"]'), 'text', 'url');
 
-$crud->setListingQuery("SELECT * FROM App_CSVGo WHERE status='1'");
+$crud->setListingQuery("SELECT * FROM App_CSVGo WHERE status='1' ORDER BY added_on DESC");
 $crud->setListingFields("name", "description", "added_on", "csv_link", 'data_link', 'status');
 
 render();
