d4713b18979b28d098f07ef2e4b90f8e3f4775a2
Binny V A <binnyva@gmail.com>
2022-05-08 17:22:23 +0530

    Added Cache reload option

--- templates/html.php
+++ templates/html.php
@@ -1,6 +1,12 @@
 <?php if($data) { ?><br /><br />
 <h1><?php echo format($name); ?></h1>
 
+<?php if(i($QUERY, 'no_cache')) { ?>
+	<p>Data Refreshed! There might be a cache at Google Sheet end, to refresh that change the CSV URL a bit. Eg. Add <code>&x=1</code> to the end of the URL. If that is already there, increment the number by 1. This will force Sheet to refresh the data.</p>
+<?php } else { ?>
+	<p><a href="index.php?mime=html&name=<?php echo $name; ?>&no_cache=1">🔄 Refresh Cache</a></p>
+<?php } ?>
+
 <?php
 if($pager) {
 	$pager->link_template = '<a href="%%PAGE_LINK%%" class="page-%%CLASS%%"><img alt="%%TEXT%%" src="images/icons/arrows/%%CLASS%%.png" /></a>';
